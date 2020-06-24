8e098b9e0dae6ef3be5bbbbbd9401ddd38a569b9
Axel-AMGHAR <axel.amghar@etu.univ-amu.fr>
2020-06-24 23:43:29 +0200

    cahnge css italic and orientation

--- trackmania_css/Trackmania_nav_be_like/resources/views/trackmania_nav.blade.php
+++ trackmania_css/Trackmania_nav_be_like/resources/views/trackmania_nav.blade.php
@@ -18,16 +18,16 @@
     }
 </style>
 <div class="h-screen flex items-center justify-center">
-    <div class="italic font-extrabold " style="transform: skewX(-8deg);">
+    <div class="font-extrabold" style="transform: skewX(-10deg);">
         <div class="rounded-tl-xl py-4 px-8 bg-green-900 text-5xl leading-none">
-            <h1 class="text-white ml-2">SUMMER 2020</h1>
+            <h1 class="text-white ml-1">SUMMER 2020</h1>
             <h1 class="text-green-300">SUMMER 2020 - 25</h1>
         </div>
         <span class="text-3xl">
             <div class="flex flex-row justify-around bg-green-600 py-4 px-8">
-                <div class="">
-                    <h2 class="text-white">RECORD PERSONEL</h2>
-                    <img class="w-20 m-auto mt-4" style="transform: skewX(8deg);" src="{{asset('img/medal.png')}}" alt="">
+                <div class="mr-16">
+                    <h2 class="text-white">RECORD PERSONNEL</h2>
+                    <img class="w-20 m-auto mt-4" style="transform: skewX(10deg);" src="{{asset('img/medal.png')}}" alt="">
                 </div>
                 <div class="flex flex-col items-end">
                     <h2 class="text-white p-1 pt-0">01:02.654</h2>
